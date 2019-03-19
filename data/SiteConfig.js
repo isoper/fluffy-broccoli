6bec5a5288dba248847005311261e4b0e6332c83
Bruno PEREIRA <bruno.pereira@brunop.eu>
2019-03-19 01:27:15 +0100

    Switch title to Ludo

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,11 +1,11 @@
 const config = {
-  siteTitle: "Orion Technology", // Site title.
-  siteTitleShort: "Orion tech.", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "Orion, tech team that flows", // Alternative site title for SEO.
+  siteTitle: "LUDŌ", // Site title.
+  siteTitleShort: "LUDŌ tech.", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "LUDŌ, tech team that flows", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://www.oriontechnology.fr", // Domain of your website without pathPrefix.
   pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "Orion, tech team that flows", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "LUDŌ, tech team that flows", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
