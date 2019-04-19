0e3b4f00ee5406b8e94303494ce1a658a38c523e
Bruno PEREIRA <bruno.pereira@brunop.eu>
2019-04-19 08:27:28 +0200

    Add GAID

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -8,7 +8,7 @@ const config = {
   siteDescription: "LUDŌ, tech team that flows", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
+  googleAnalyticsID: "UA-138679635-1", // GA tracking ID.
   disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
