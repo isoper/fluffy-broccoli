4ad30e5ad5552fc10d4d5a8c4319df3d30645dd6
Bruno PEREIRA <bruno.pereira@brunop.eu>
2019-04-19 07:22:39 +0200

    Add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,13 +97,8 @@ module.exports = {
         display: "minimal-ui",
         icons: [
           {
-            src: "/logos/logo-192x192.png",
+            src: "/logos/icon-192.png",
             sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/logos/logo-512x512.png",
-            sizes: "512x512",
             type: "image/png"
           }
         ]
