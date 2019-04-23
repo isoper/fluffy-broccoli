78880d1e195886dedecbf76d943fff9e1b53537b
Bruno PEREIRA <bruno.pereira@brunop.eu>
2019-04-23 08:39:28 +0200

    Remove autolink headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,6 @@ module.exports = {
           },
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
-          "gatsby-remark-autolink-headers",
           "gatsby-remark-twemoji"
         ]
       }
