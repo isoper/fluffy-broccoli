964ef4a5b93b5054de03b36cdfc1e90ba5673ad4
Vili <vtu@nix.mk>
2019-12-16 10:18:51 +0100

    removed layout in jobs

--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -1,21 +1,7 @@
 import React from "react";
-import Helmet from "react-helmet";
-import { graphql } from "gatsby";
-
-import Layout from "../layout";
-import JobsHeader from "../components/JobsHeader";
-import Values from "../components/Values";
-import OurOffer from "../components/OurOffer";
-import WhereAreWe from "../components/WhereAreWe";
-import JobsCard from "../components/JobCards";
-
-const JobsPage = ({ data }) => {
-
+const JobsPage = () => {
   return (
-    <Layout>
-      <div className="index-container">
-      </div>
-    </Layout>
+    <div />
   );
 };
 
